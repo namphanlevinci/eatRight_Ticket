df37d1989fd00baf8a328a15ca47ea3e7f5ae032
Anhht1808 <150642381+Anhht1808@users.noreply.github.com>
2024-09-17 17:47:00 +0700

    Update index.ts

--- src/graphql/printer/index.ts
+++ src/graphql/printer/index.ts
@@ -39,3 +39,27 @@ export const DELETE_KITCHEN_STATION = gql`
         deleteKitchenStation(id: $id)
     }
 `;
+
+export const SELECT_PRINTER_DEVICE = gql`
+    mutation ($printer_id: Int!) {
+        merchantConfigPrinter(printer_id: $printer_id)
+    }
+`;
+
+export const PRINT_BILL = gql`
+    mutation ($invoice_number: String!) {
+        merchantPrintReceipt(invoice_number: $invoice_number)
+    }
+`;
+
+export const LIST_PRINTER_DEVICES = gql`
+    {
+        merchantGetListDevice {
+            restaurant_id
+            prints {
+                id
+                printer_name
+            }
+        }
+    }
+`;
