e7d98726eb97c6957b4cdd1246aff33f93d5eae7
Anhht1808 <150642381+Anhht1808@users.noreply.github.com>
2024-09-20 14:54:47 +0700

    fix to build

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import notification_sound from '../assets/sound/notification.mp3';
+import notification_sound from 'assets/sound/notification.mp3';
 
 export const playNotiSound = () => {
     const audio = new Audio(notification_sound);
