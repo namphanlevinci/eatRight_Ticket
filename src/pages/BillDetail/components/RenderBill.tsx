4d8fe379b9bdb79fad4f2b9281862eb7a674058a
Anhht1808 <150642381+Anhht1808@users.noreply.github.com>
2024-09-21 11:32:02 +0700

    render status table

--- src/pages/BillDetail/components/RenderBill.tsx
+++ src/pages/BillDetail/components/RenderBill.tsx
@@ -237,20 +237,12 @@ const RenderBillItem = ({
                 {data?.payment_methods &&
                     data?.payment_methods[0]?.additional_data[1]?.value && (
                         <RowStyled align={'middle'}>
-                            <TextDark style={text16}>Cart Type:</TextDark>
-                            <TextDark>
+                            <TextDark style={text16}>
                                 {
                                     data?.payment_methods[0]?.additional_data[1]
                                         ?.value
                                 }
-                            </TextDark>
-                        </RowStyled>
-                    )}
-                {data?.payment_methods &&
-                    data?.payment_methods[0]?.additional_data[0]?.value && (
-                        <RowStyled align={'middle'}>
-                            <TextDark style={text16}>Last 4 Digits:</TextDark>
-                            <TextDark>
+                                {'  '}
                                 {
                                     data?.payment_methods[0]?.additional_data[0]
                                         ?.value
@@ -258,6 +250,7 @@ const RenderBillItem = ({
                             </TextDark>
                         </RowStyled>
                     )}
+
                 <DividedDashed />
                 <TextDark>
                     <TextDark style={text16}>
