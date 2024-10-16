f0416bfad407618f1eecf94ebd3e9e2837399610
Phan Nhật Nam <84962280+namphanlevinci@users.noreply.github.com>
2024-10-16 14:00:51 +0700

    UI Merchant setting

--- src/pages/RestaurentManagement/General/GeneralPage.tsx
+++ src/pages/RestaurentManagement/General/GeneralPage.tsx
@@ -1,15 +1,19 @@
-import { notification, Switch } from 'antd';
+import { notification, Switch, Tooltip } from 'antd';
 import { Text } from 'components/atom/Text';
 import { RowStyled } from 'pages/BillDetail/styled';
 import React, { useEffect } from 'react';
+import QuestionIcon from 'assets/icons/questionIcon';
 
 export default function RestaurentGeneralPage() {
     const [isTableView, setIsTableView] = React.useState(false);
+    const [isOpenPrice, setOpenPrice] = React.useState(false);
+
     useEffect(() => {
         setIsTableView(
             JSON.parse(localStorage.getItem('isTableView') || 'false'),
         );
     }, []);
+
     const handleSetDefaultTableView = (value: boolean) => {
         setIsTableView(value);
         notification.success({
@@ -19,17 +23,62 @@ export default function RestaurentGeneralPage() {
 
         localStorage.setItem('isTableView', JSON.stringify(value));
     };
+
+    const handleSetOpenPrice = (value: boolean) => {
+        setOpenPrice(value);
+        localStorage.setItem('isOpenPrice', JSON.stringify(value));
+    };
+
     return (
         <div style={{ padding: 16 }}>
-            <Text style={{ fontSize: 18, fontWeight: 600 }}>
-                Restaurant General
-            </Text>
+            <div
+                style={{
+                    borderBottom: '1px solid #dddddd',
+                    paddingBottom: 16,
+                    marginBottom: 16,
+                }}
+            >
+                <Text style={{ fontSize: 18, fontWeight: 600 }}>
+                    Restaurant General
+                </Text>
+            </div>
 
-            <RowStyled style={{ maxWidth: 600 }}>
+            <RowStyled
+                style={{
+                    borderBottom: '1px solid #dddddd',
+                    paddingBottom: 16,
+                    marginBottom: 16,
+                }}
+            >
                 <Text>Default Table View</Text>
                 <Switch
                     value={isTableView}
                     onChange={(value) => handleSetDefaultTableView(value)}
+                />
+            </RowStyled>
+            <RowStyled
+                style={{
+                    borderBottom: '1px solid #dddddd',
+                    paddingBottom: 16,
+                    marginBottom: 16,
+                }}
+            >
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Text>Open Pricing for All Items</Text>
+                    <div
+                        style={{
+                            position: 'relative',
+                            marginLeft: 16,
+                            marginTop: 5,
+                            cursor: 'pointers',
+                        }}
+                    >
+                        <QuestionIcon />
+                    </div>
+                </div>
+                <Switch
+                    value={isOpenPrice}
+                    onChange={(value) => handleSetOpenPrice(value)}
                 />
             </RowStyled>
         </div>
